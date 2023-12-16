139555ac01971d551c2bb129e39f6a97824d1686
Pixel_Wizard <2246063@sliet.ac.in>
2023-12-17 01:26:18 +0530

    rmv

--- Img_Crop/Crop.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import matplotlib.pyplot as plt
-img = cv2.imread("cat.jpg")
-crop_image=img[150:150 ,100:300]
-#cv2.imshow(crop_image)
-plt.figure(figsize=(10,10))
-plt.imshow(crop_image)
-plt.show()
